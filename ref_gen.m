4364052bfbdbd28dd7609db9c3f1478353168c46
Sesha <scharla@purdue.edu>
2024-04-22 15:08:21 -0400

    Got the thing right

--- ref_gen.m
+++ ref_gen.m
@@ -1,6 +1,6 @@
 clear; clc;
 
-wr = 100;
+wr = 250;
 Fs = 250;
 Ts = 1/Fs;
 
@@ -9,3 +9,18 @@ G = tf([wr^2], [1, sqrt(2)*wr, wr^2]);
 s = tf([1, 0], [0, 1]);
 Gd = c2d(G, Ts, 'zoh');
 sGd = c2d(s*G, Ts, 'zoh');
+
+% bode plots
+figure()
+bode(G)
+hold on
+bode(Gd)
+hold off
+legend()
+
+figure()
+bode(G*s)
+hold on
+bode(sGd)
+hold off
+legend()
